3f6c80f8e974f93d329b17c7c0fcbbeed831d606
Mirko Ioris <76486521+Mikkos18@users.noreply.github.com>
2022-02-03 16:45:12 +0200

    fixed colors, input tolerance, print functions

--- ns-3.35/custom_network.py
+++ ns-3.35/custom_network.py
@@ -3,6 +3,7 @@ from bcolors import bcolors
 from custom_packet import CustomPacket
 from custom_router import CustomRouter
 from custom_tcp_message import ETCP_MSG_TYPE, CustomTcpMessage
+import re  # to check input syntax
 
 
 class CustomNetwork:
@@ -18,16 +19,21 @@ class CustomNetwork:
 
     # idx1 and idx2 are for testing purposes
     def add_link(self, idx1=None, idx2=None):
+        print(bcolors.OKCYAN)
         self._printRouters()
-        if (idx1 != None and idx2 != None):
+        print(bcolors.ENDC)
+        if not self.routers:
+            return
+
+        if idx1 != None and idx2 != None:
             first_router_idx = idx1
             second_router_idx = idx2
         else:
             first_router_idx = input('First router index:')
             second_router_idx = input('Second router index:')
 
-        if (int(first_router_idx) >= self.get_network_size() or int(second_router_idx) >= self.get_network_size()):
-            print("Invalid indexes... returning")
+        if int(first_router_idx) >= self.get_network_size() or int(second_router_idx) >= self.get_network_size():
+            print(bcolors.FAIL + "Invalid indexes... returning" + bcolors.ENDC)
             return
         else:
             first_router = self.routers[int(first_router_idx)]
@@ -35,24 +41,46 @@ class CustomNetwork:
             first_router.send_tcp_msg(
                 _to=second_router, msg=CustomTcpMessage(type=ETCP_MSG_TYPE.SYN))
             self.links.append((first_router, second_router))
-            print('Link created between the two router')
+            print(bcolors.OKGREEN + 'Link created between the two router' + bcolors.ENDC)
 
     def remove_link(self):
-        self._printLinks()
-        selected_idx = input('Link to remove: ')
-        if int(selected_idx) >= len(self.links):
-            print('Wrong index!')
+        if not self.links:
+            print('There are no more links to remove!')
             return
-        selected_link = self.links[int(selected_idx)]
-        router1: CustomRouter = selected_link[0]
-        router2: CustomRouter = selected_link[1]
-        router1.send_tcp_msg(
-            _to=router2, msg=CustomTcpMessage(type=ETCP_MSG_TYPE.FIN))
-        self.links.pop(int(selected_idx))
+
+        print(bcolors.OKCYAN)
+        self._printLinks()
+        print(bcolors.ENDC)
+        selected_idx = input('Link to remove: ')
+
+        checkIdx = re.compile(r'\d(\d)?')  # allow only decimals
+        if checkIdx.match(selected_idx):  # if the user inserted numbers
+            selected_idx = int(selected_idx)  # we convert the string to int
+
+            if selected_idx >= len(self.links):
+                print(bcolors.FAIL + 'Wrong index!' + bcolors.ENDC)
+                return
+            else:
+                selected_link = self.links[int(selected_idx)]
+                router1: CustomRouter = selected_link[0]
+                router2: CustomRouter = selected_link[1]
+                router1.send_tcp_msg(
+                    _to=router2, msg=CustomTcpMessage(type=ETCP_MSG_TYPE.FIN))
+                self.links.pop(int(selected_idx))
+                print(bcolors.OKGREEN + 'Link successfully removed!' + bcolors.ENDC)
+        else:
+            print(bcolors.FAIL + 'Insert only numbers!' + bcolors.ENDC)
+            return
 
     def remove_router(self):
-        self.print_network()
-        sr = input('Select which router you want to remove: ')
+        if not self.routers:
+            print('There are no more routers to remove!')
+            return
+        print(bcolors.OKCYAN)
+        self._printRouters()
+        print(bcolors.ENDC)
+        # TO DO: when removing a router, delete its links to other routers and the rows in the routing table
+        sr = input('Select which router you want to remove (press a letter to exit): ')
         if sr.isdigit() and int(sr) >= 0 and int(sr) <= len(self.routers) - 1:
             self.routers.pop(int(sr))
             print(bcolors.OKGREEN + "The router was successfully removed!" + bcolors.ENDC)
@@ -60,38 +88,55 @@ class CustomNetwork:
             print(bcolors.FAIL + "The given index is not correct!" + bcolors.ENDC)
 
     def print_network(self):
-        print(bcolors.OKCYAN + "Current network topology:")
+        print(bcolors.OKCYAN + "Current network topology:\n")
         self._printRouters()
         self._printLinks()
         self._printTables()
         print(bcolors.ENDC)
 
     def _printRouters(self):
-        print("\nRouters")
+        print("Routers")
+        if not self.routers:
+            print("There are no routers, add one by selecting command 2!\n\n")
+            return
         for i, r in enumerate(self.routers):
-            print(f'{i}: {r.name} - {r.ip_address}')
-            print(f'    Connected with:')
-            for l in r.links:
-                print(f'      - {l.name}\n')
+            print(f'\n{i}: {r.name} - {r.ip_address}')
+            if not r.links:
+                continue
+            else:
+                print(f'   Connected with:')
+                for l in r.links:
+                    print(f'     - {l.name}')
+        print('\n')
 
     def _printLinks(self):
-        print("\nLinks")
+        print("Links")
+        if not self.links:
+            print("There are no links, create one by selecting command 4!\n\n")
+            return
         for i, pairs in enumerate(self.links):
-            print(f'{i}: {pairs[0].name} - {pairs[1].name}\n')
+            print(f'{i}: {pairs[0].name} - {pairs[1].name}')
+        print('\n')
 
     def _printTables(self):
-        print("\nRouting Tables")
+        print("Routing Tables")
+        if not self.routers:
+            print("Without routers there can be none of these!\n")
+            return
         for i, r in enumerate(self.routers):
-            print(f'{r.name} - {r.ip_address}')
+            print(f'\n{r.name} - {r.ip_address}')
             r.print_tables()
 
     def send_packet(self):
         print("Sending packet from router to router")
-        self.print_network()
+        print(bcolors.OKCYAN)
+        self._printRouters()
+        print(bcolors.ENDC)
+
         inp = input("Index of the source router:")
-        ip_to = input("Destination Ip address:") #MAYBE WE CAN USE THE INDEX HERE TOO?
+        ip_to = input("Destination Ip address:")  # MAYBE WE CAN USE THE INDEX HERE TOO?
         if int(inp) >= 0 and int(inp) < len(self.routers):
             selected_router = self.routers[int(inp)]
             selected_router.send_packet(CustomPacket(selected_router.ip_address, ip_to))
         else:
-            print("The selected router index is not valid! Try again!")
+            print(bcolors.FAIL + "The selected router index is not valid! Try again!" + bcolors.ENDC)
