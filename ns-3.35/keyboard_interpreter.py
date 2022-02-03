b5470ffa5c1f781033972bcf4c356821b99828b1
Mirko Ioris <76486521+Mikkos18@users.noreply.github.com>
2022-02-03 16:41:58 +0200

    changed syntax and colors

--- ns-3.35/keyboard_interpreter.py
+++ ns-3.35/keyboard_interpreter.py
@@ -1,19 +1,20 @@
 from bcolors import bcolors
 
+
 # Read command from user. Also check the validity of the given number
 def read_command():
     while True:
         userinput = input('Select command (8 for help):')
         if not userinput.isdigit():
             print(bcolors.FAIL +
-                  "The input is not number! Please try again!" + bcolors.ENDC)
+                  "The input is not a number! Please try again!" + bcolors.ENDC)
             continue
 
         userinput_num = int(userinput)
-        if (userinput_num < 1 or userinput_num > 9):
+        if userinput_num < 1 or userinput_num > 9:
             print(
                 bcolors.FAIL + "The selected command is out of range! Please try again!" + bcolors.ENDC)
             continue
 
-        print('Thank you! The selected command is: ' + str(userinput_num)+'\n')
+        print(bcolors.OKBLUE + 'Thank you! The selected command is: ' + str(userinput_num) + '\n' + bcolors.ENDC)
         return userinput_num
