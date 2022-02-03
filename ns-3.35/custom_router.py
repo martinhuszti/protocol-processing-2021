6c7a9d71ca8feab3ee6ee11f534af5e0de445b51
Mirko Ioris <76486521+Mikkos18@users.noreply.github.com>
2022-02-03 16:46:52 +0200

    better print

--- ns-3.35/custom_router.py
+++ ns-3.35/custom_router.py
@@ -1,9 +1,11 @@
 from queue import Empty
 from custom_packet import CustomPacket
 from custom_tcp_message import ETCP_MSG_TYPE, CustomTcpMessage
+from bcolors import bcolors
 
 from random import random
-from prettytable import PrettyTable # to print routing tables in a cute way
+from prettytable import PrettyTable  # to print routing tables in a cute way
+
 
 class CustomRouter:
     def __init__(self, name='No name given', ip_address="255.255.255.255", subnet=32):
@@ -17,15 +19,18 @@ class CustomRouter:
         print(self.name)
 
     def print_tables(self):
-        t = PrettyTable(['Destination','Netmask','Metric'])
+        t = PrettyTable(['Destination', 'Netmask', 'Metric'])
+        if not self.routing_table:
+            print('This table is currently empty!')
+            return
         for r in self.routing_table:
             t.add_row([r[0], r[1], r[2]])
-        print(f'{t}\n')
+        print(t)
 
     def send_tcp_msg(self, _to, msg: CustomTcpMessage):
         self.current_seq_num = random()
         # If syn, generate seq number otherwise it is containing already
-        if(msg.type == ETCP_MSG_TYPE.SYN or msg.type == ETCP_MSG_TYPE.FIN):
+        if msg.type == ETCP_MSG_TYPE.SYN or msg.type == ETCP_MSG_TYPE.FIN:
             msg.seq_num = self.current_seq_num
             print(f'{self.name}: sending {msg.type.name} message to {_to.name}')
         # Simulate that the message is arrived to the router
@@ -39,28 +44,31 @@ class CustomRouter:
         if _type == ETCP_MSG_TYPE.SYN:
             print(f'{self.name}: sending back syn-ack with ')
             self.send_tcp_msg(_to=_from, msg=CustomTcpMessage(
-                type=ETCP_MSG_TYPE.SYN_ACK, seq_num=tcp_message.seq_num, ip_address=self.ip_address, subnet=self.subnet))
+                type=ETCP_MSG_TYPE.SYN_ACK, seq_num=tcp_message.seq_num, ip_address=self.ip_address,
+                subnet=self.subnet))
 
         # 2. Answer with SYN-ACK and send the ip range
         if _type == ETCP_MSG_TYPE.SYN_ACK:
-            if(tcp_message.seq_num == self.current_seq_num):
+            if tcp_message.seq_num == self.current_seq_num:
                 print(
                     f"{self.name}: The sequence number is correct, initializing connection")
                 self.send_tcp_msg(
                     _from, CustomTcpMessage(type=ETCP_MSG_TYPE.ACK))
                 self.current_seq_num = 0
                 self.links.append(_from)
-                self.routing_table.append((_from.ip_address, _from.subnet, 1)) #1 is the hop -> TO DO: create a better function to determine metrics
+                self.routing_table.append((_from.ip_address, _from.subnet, 1))  # 1 is the hop ->
+                # TO DO: create a better function to determine metrics
 
         if _type == ETCP_MSG_TYPE.ACK:
-            if(tcp_message.is_fin_ack_response == True):
+            if tcp_message.is_fin_ack_response:
                 print(
                     f"{self.name}: ACK arrived to the FIN_ACK message. Removing the link...")
                 self.links.remove(_from)
             else:
                 print(f"{self.name}: Initializing connection")
                 self.links.append(_from)
-                self.routing_table.append((_from.ip_address, _from.subnet, 1)) #1 is the hop -> TO DO: create a better function to determine metrics
+                self.routing_table.append((_from.ip_address, _from.subnet, 1))  # 1 is the hop ->
+                # TO DO: create a better function to determine metrics
 
         if _type == ETCP_MSG_TYPE.FIN_ACK:
             self.links.remove(_from)
@@ -79,14 +87,16 @@ class CustomRouter:
                 type=ETCP_MSG_TYPE.FIN_ACK))
 
     def send_packet(self, packet: CustomPacket):
-        if(packet.ip_from == self.ip_address):
+        if packet.ip_from == self.ip_address:
             print(f"{self.name}: I'm sending a packet to: {packet.ip_to}")
         else:
             print(f"{self.name}: A new packet arrived.")
 
         if packet.ip_to == self.ip_address:
             print(
-                f"{self.name}: The destination of the packet is me! It's arrived to the destination! {self.ip_address}")
+                f"{self.name}:" + bcolors.OKGREEN +
+                f" The destination of the packet is me! It's arrived to the destination! {self.ip_address}"
+                + bcolors.ENDC)
             return
 
         found_router = Empty
@@ -94,8 +104,8 @@ class CustomRouter:
             if link.ip_address == packet.ip_to:
                 found_router = link
                 break
-            
+
         if found_router != Empty:
             found_router.send_packet(packet)
         else:
-            print(f"{self.name}: Did not found the target router! I'm sorryyyy")
+            print(f"{self.name}:" + bcolors.FAIL + " Error! The destination is unreachable!" + bcolors.ENDC)
