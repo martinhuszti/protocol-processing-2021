41aac904e44b379f30fbf465fbb2695bba61d9ef
Victor Schmit <victor.schmit.vs@gmail.com>
2022-06-12 17:26:54 +0300

    WIP improvement of Simulation

--- main_project/simulation.py
+++ main_project/simulation.py
@@ -7,31 +7,42 @@ from custom_packet import CustomPacket
 
 def start_simulation():
     network = CustomNetwork()
+    print("Welcome to the simulation!\n\n")
+
+    print("#####################################################\n")
+    print("Creation of three basic AS\n")
+    print("#####################################################\n")
     network.add_router(create_custom_router('AS_1', "100.0.1.0", 24))
     network.add_router(create_custom_router('AS_2', "100.0.2.0", 24))
     network.add_router(create_custom_router('AS_3', "100.0.3.0", 24))
     network.add_router(create_custom_router('AS_4', "100.0.4.0", 24))
+
+    print("#####################################################\n")
+    print("Creation of three links\n")
+    print("#####################################################\n")
+
+
     network.add_link(0, 1)
     network.add_link(0, 2)
     network.add_link(2, 3)
 
-    ## first syn synack ack 
+
+    print("#####################################################\n")
+    print("Trying to send a packet from AS_1 to AS_2. Direct link available\n")
+    print("#####################################################\n")
+
+    network.routers[0].send_packet(CustomPacket(network.routers[0].ip_address,network.routers[1].ip_address))
     
-    # for routers in network.routers:
-    #     for other_routers in network.routers:
-    #         if routers != other_routers and network.routers.index(other_routers)>network.routers.index(routers):
-    #             routers.send_tcp_msg(other_routers, CustomTcpMessage(ETCP_MSG_TYPE.SYN))
-    #             other_routers.receive_tcp_msg(routers, CustomTcpMessage(ETCP_MSG_TYPE.SYN))
-    #             routers.receive_tcp_msg(other_routers, CustomTcpMessage(ETCP_MSG_TYPE.SYN_ACK))
-    #             other_routers.receive_tcp_msg(routers, CustomTcpMessage(ETCP_MSG_TYPE.ACK))
-    
-    #network.routers[0].send_packet(CustomPacket(network.routers[0].ip_address,network.routers[1].ip_address))
-    #network.routers[0].send_packet(CustomPacket(network.routers[0].ip_address,network.routers[3].ip_address))
+    print("#####################################################\n")
+    print("Trying to send a packet from AS_1 to AS_4. No direct link available\n")
+    print("#####################################################\n")  
+
+    network.routers[0].send_packet(CustomPacket(network.routers[0].ip_address,network.routers[3].ip_address))
     
 
     ###########################For dev purposes, NEEDS TO BE CHANGED###########################
-    #answer = input("Do you want to keep the current network? (y/n)")
-    answer = "y"
+    answer = input("Do you want to keep the current network? (y/n)")
+
     ###########################################################################################
     if answer.lower() == "y":
         return network
